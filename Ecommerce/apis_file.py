4953a4e38e60c32a4787db9b5c336dc78945b8c2
mofahmi99 <m.fahmy@sary.com>
2021-08-31 05:02:12 +0200

    updated the urls to add categories apis

--- Ecommerce/apis_file.py
+++ Ecommerce/apis_file.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('product/', include("products.api.urls")),
+    path('products/', include("products.api.urls")),
+    path('categories/', include("products.api.urls")),
     path('user/', include("users.api.urls")),
     path('cart/', include("carts.api.urls")),
     path('order/', include("orders.api.urls")),
-    # path('branch/', include("branches.api.urls")),
+
 
 ]
