46eeaf7fa25cb7806c2bedfa9fed2265cb668019
mofahmi99 <m.fahmy@sary.com>
2021-08-31 04:44:46 +0200

    removed city field from user model and added it to address model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.base_user import AbstractBaseUser
 class User(AbstractBaseUser, PermissionsMixin):
     """
     User model is used to hold information about employees that uses the system such as  username, password,email and phonenumber
-
     """
     user_name = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
@@ -16,7 +15,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     token = models.CharField(max_length=255, null=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    address = models.ForeignKey('locations.Location', on_delete=models.CASCADE, default=1)
 
     USERNAME_FIELD = 'user_name'
     objects = UserManager()
