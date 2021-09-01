ca3ed5f404d6e6bc4dc45f66efe94469e96b984b
mofahmi99 <m.fahmy@sary.com>
2021-09-01 05:40:29 +0200

    retrieve products based on its availability in user location

--- branches/api/urls.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework.routers import DefaultRouter
-# from branches.api import views
-#
-# router = DefaultRouter()
-# router.register('', views.BranchApi, basename='branch')
-# urlpatterns = router.urls
-#
-#
