e7b9572c96f8af4829bbdb3484d33a6e0fe2717d
mofahmi99 <m.fahmy@sary.com>
2021-09-01 05:58:05 +0200

    implemented calculate_price function to return item best price

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,10 @@
 from django.core.mail import send_mail
 
 
+"""
+send email to the user after order is created 
+"""
+# email data
 subject = 'your order is ready'
 body = 'thank you'
 sender_mail = 'slymodi88@gmail.com'
@@ -15,6 +19,16 @@ def send_order_email(email):
     )
 
 
+def calculate_price(branch_items):
+    """
+    calculate price according to available items if not return min price of items
+    :param branch_items: branch_items is a queryset that each items in it must contains is_available attribute
+    :return: Decimal price
+    """
+    available_prices = [branch_item.price for branch_item in branch_items if branch_item.is_available]
+    if available_prices:
+        return min(available_prices)
+    return min([branch_item.price for branch_item in branch_items])
 
 
 
@@ -27,44 +41,3 @@ def send_order_email(email):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def get_total_cart(instance):
-#     from carts.models import CartItem
-#     total = 0
-#     print(instance.shipping_fee)
-#     items = CartItem.objects.filter(cart_id=instance.id)
-#     for i in items:
-#         total += i.quantity * i.item.price
-#
-#     if total > 100:
-#         instance.shipping_fee = 0
-#     else:
-#         instance.shipping_fee = 20
-#
-#     instance.grand_total = total + instance.shipping_fee
-#     print(instance.grand_total)
-
-# return total
